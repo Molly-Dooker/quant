620aa7e8209086e5493bc70bb22a490c45fc1cff
lsm0729 <lsm0729@bos-semi.com>
2025-04-10 14:25:16 +0900

    ..

--- venv.sh
+++ venv.sh
@@ -1,5 +1,5 @@
 # python 3.10.12
-pip install torch torchvision torchaudio  # torch 2.6.0
+pip install torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cu124
 pip install ultralytics==8.3.97
 pip install optimum==1.24.0
 pip install optimum-quanto==0.2.7
@@ -11,4 +11,5 @@ pip install loguru==0.7.3
 pip install tabulate==0.9.0
 pip install ipdb
 pip install supervision==0.25.1
+pip install albumentations==2.0.5
 # aimet-torch 2.1.0+cu121
