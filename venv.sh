af55f87b766e1dc2e813845315aae6861152b4e1
lsm0729 <lsm0729@bos-semi.com>
2025-04-10 14:42:40 +0900

    ..

--- venv.sh
+++ venv.sh
@@ -1,15 +1,15 @@
 # python 3.10.12
 pip install torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cu124
-pip install ultralytics==8.3.97
+pip install transformers==4.51.1
+pip install accelerate==1.6.0
 pip install optimum==1.24.0
 pip install optimum-quanto==0.2.7
+pip install ultralytics==8.3.97
 pip install scikit-learn==1.6.1
-pip install transformers==4.49.0
 pip install evaluate==0.4.3
-pip install accelerate==1.5.2s
+pip install supervision==0.25.1
+pip install albumentations==2.0.5
 pip install loguru==0.7.3
 pip install tabulate==0.9.0
 pip install ipdb
-pip install supervision==0.25.1
-pip install albumentations==2.0.5
 # aimet-torch 2.1.0+cu121
