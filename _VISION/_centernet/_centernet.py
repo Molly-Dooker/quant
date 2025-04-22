904e198950561cfa2920ed6c0719d2c9bd6532e3
lsm0729 <lsm0729@bos-semi.com>
2025-04-22 11:52:55 +0900

    ..

--- _VISION/_centernet/_centernet.py
+++ _VISION/_centernet/_centernet.py
@@ -124,12 +124,11 @@ class DCN(DCNv2):
 class DeformConv(nn.Module):
     def __init__(self, chi, cho):
         super(DeformConv, self).__init__()
+        self.conv = DCN(chi, cho, kernel_size=(3,3), stride=1, padding=1, dilation=1, deformable_groups=1)
         self.actf = nn.Sequential(
             nn.BatchNorm2d(cho, momentum=BN_MOMENTUM),
             nn.ReLU(inplace=True)
         )
-        self.conv = DCN(chi, cho, kernel_size=(3,3), stride=1, padding=1, dilation=1, deformable_groups=1)
-
     def forward(self, x):
         x = self.conv(x)
         x = self.actf(x)
