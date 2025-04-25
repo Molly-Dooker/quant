968ed9c4afb9421a10b41a6da2e84ad84a5a1a7a
lsm0729 <lsm0729@bos-semi.com>
2025-04-25 10:33:36 +0900

    ..

--- _VISION/_resnet/qat2.py
+++ _VISION/_resnet/qat2.py
@@ -95,35 +95,6 @@ def calibrate(model, device, dataloader, num=10000):
 
 
 
-
-act_observer = MovingAverageMinMaxObserver.with_args(
-    quant_min=-128, quant_max=127,
-    dtype=torch.qint8,
-    qscheme=torch.per_tensor_symmetric
-)
-wt_observer = MovingAveragePerChannelMinMaxObserver.with_args(
-    quant_min=-128, quant_max=127,
-    dtype=torch.qint8,
-    qscheme=torch.per_channel_symmetric
-)
-
-learnable_act = LearnableFakeQuantize.with_args(
-    ovserver = act_observer,
-    quant_min=-128,
-    quant_max=127,
-    dtype=torch.qint8,
-    use_grad_scaling=True,
-)
-
-
-learnable_wt = LearnableFakeQuantize.with_args(
-    ovserver = wt_observer,
-    quant_min=-128,
-    quant_max=127,
-    dtype=torch.qint8,
-    use_grad_scaling=True,
-)
-
 from torch.ao.quantization import (
     get_default_qconfig,
     get_default_qat_qconfig,
@@ -137,11 +108,14 @@ from torch.ao.quantization import (
     HistogramObserver,
     PerChannelMinMaxObserver,
     MinMaxObserver,
-    default_per_channel_qconfig
+    default_per_channel_qconfig,
+    get_default_qat_module_mappings
 )
 from torch.ao.quantization.quantize_fx import (
     prepare_fx, 
-    convert_fx
+    convert_fx,
+    prepare_qat_fx,
+    
     )
 
 def main(args):
@@ -155,33 +129,56 @@ def main(args):
     dataloader = torch.utils.data.DataLoader(prepared_ds, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
 
-    
-    qat_config_ = get_default_qat_qconfig(version=0)
-    qat_config  = QConfig(activation=learnable_act, weight=learnable_wt)
-    model.fc.qconfig = qat_config_
-    # 이건 quantization parameter 는 학습되지 않음. 오직 weight 만 변경됨
+  
+    learnable_act = LearnableFakeQuantize.with_args(
+        observer=HistogramObserver,
+        quant_min=-128,
+        quant_max=127,
+        dtype=torch.qint8,
+        qscheme=torch.per_tensor_symmetric,
+        use_grad_scaling=True,
+    )    
+    # fakequantize 와 다르게 LearnableFakeQuantize 는 channel 값을 지정해줘야됨. 
+    # fakequantize 는 scale 과 zp의 shape 이 안맞으면 알아서 맞게 바꿔주는데 이거는 
+    # 왜 그런지 모르겠는데 그 기능이 추가 안되있음.
+    learnable_weights = lambda channels:LearnableFakeQuantize.with_args(
+        observer=MovingAveragePerChannelMinMaxObserver,
+        quant_min=-128,
+        quant_max=127,
+        dtype=torch.qint8,
+        qscheme=torch.per_channel_symmetric,
+        use_grad_scaling=True,
+        channel_len = channels
+    )          
+          
+    qconfig = lambda channels : tq.QConfig(
+        activation=learnable_act,
+        weight=learnable_weights(channels))
+    model.fc.qconfig = qconfig(model.fc.out_features)
+    model.conv1.qconfig = qconfig(model.conv1.out_channels)
     model.train()
     prepare_qat(model,inplace=True)
-    ipdb.set_trace()
-    # fc 는 torch.ao.nn.qat.modules.linear.Linear 가 사용됨.
-    # 만약 torch.ao 에서 기본적으로 제공하는 함수가 아닌 다른 함수 (ex deformconv) 라면 커스텀 모듈 만들어야 될듯.
-    model.train()
+    
     model.to(args.device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr = 0.1) # weight 및 param 이 변하는지 보기 위해 큰값을 적용함.
-    
+    optimizer = torch.optim.SGD(model.parameters(), lr = 0.1) # weight, qparam 변화 확인위해 일부로 큰값 줌
+    # 확인용
+    fc_weight = model.fc.weight.clone().detach()
+    fc_scale  = model.fc.weight_fake_quant.scale.clone().detach()
+    conv_weight = model.conv1.weight.clone().detach()
+    conv_scale  = model.conv1.weight_fake_quant.scale.clone().detach()
     for batch in dataloader:
         data, target = batch["pixel_values"], batch["labels"]
         data= data.to(args.device)            
         output = model(data)
         
-        target[:]=4 # loss 증가위해 일부로
+        target[:]=4 # loss 극대화 하기 위해 다른값 줌
         target = target.to(args.device)
-        
         loss = criterion(output, target)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        ipdb.set_trace()
         
         
         
