8ea86dadc697af624dcb41a588250736bb961c8b
lsm0729 <lsm0729@bos-semi.com>
2025-05-16 09:23:20 +0900

    ..

--- _LLM/venv.sh
+++ _LLM/venv.sh
@@ -1,6 +1,6 @@
 # python 3.11.11
 uv pip install torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cu124
-# pip install llmcompressor==0.5.0
-# pip install lm-eval==0.4.8
-# pip install loguru==0.7.3
-# pip install ipdb==0.13.13
+uv pip install llmcompressor==0.5.1
+uv pip install lm-eval==0.4.8
+uv pip install loguru==0.7.3
+uv pip install ipdb==0.13.13
