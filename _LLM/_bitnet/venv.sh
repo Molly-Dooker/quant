83b4ce9f880a7314ff7c2568443076265445340d
lsm0729 <lsm0729@bos-semi.com>
2025-05-19 14:03:47 +0900

    ..

---
+++ _LLM/_bitnet/venv.sh
@@ -0,0 +1,4 @@
+uv pip install torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cu124
+uv pip install git+https://github.com/huggingface/transformers.git@096f25ae1f501a084d8ff2dcaf25fbc2bd60eba4
+uv pip install accelerate==1.7.0
+uv pip install setuptools==80.7.1
