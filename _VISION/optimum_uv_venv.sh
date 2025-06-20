acc5910108a94384a5589e2a527eb25f788cc00e
lsm0729 <lsm0729@bos-semi.com>
2025-06-20 09:01:07 +0900

    ..

---
+++ _VISION/optimum_uv_venv.sh
@@ -0,0 +1,21 @@
+# python 3.10.12
+uv pip install torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cu124
+uv pip install transformers==4.51.3
+uv pip install accelerate==1.8.0
+uv pip install optimum==1.26.1
+uv pip install optimum-quanto==0.2.7
+uv pip install ultralytics==8.3.97
+uv pip install scikit-learn==1.6.1
+uv pip install evaluate==0.4.3
+uv pip install supervision==0.25.1
+uv pip install albumentations==2.0.5
+uv pip install loguru==0.7.3
+uv pip install tabulate==0.9.0
+uv pip install ipdb==0.13.13
+uv pip install pycocotools==2.0.8
+uv pip install wandb==0.19.9
+uv pip install torchmetrics==1.7.1
+uv pip install timm==1.0.15
+uv pip install jupyter==1.1.1
+uv pip install onnx==1.18.0
+uv pip install onnxruntime==1.22.0
